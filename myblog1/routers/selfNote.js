75a487e96db3998ca8d72c1096f6f4c50a10823b
Jay <2752402930@qq.com>
2018-04-22 18:09:53 +0800

    delete myblog1

--- myblog1/routers/selfNote.js
+++
@@ -1,8 +0,0 @@
-const router = require('koa-router')();
-const userModel = require('../lib/mysql');
-const checkUser = require('../midllewares/checkUser');
-const moment = require('moment');
-
-
-//个人日记
-module.exports = router;
